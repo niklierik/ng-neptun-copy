e190824c6a1f043f5335c99597c9db66137e9edd
niklierik <erik200203@gmail.com>
2023-04-20 11:21:42 +0200

    Created login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Injectable({
     providedIn: "root",
 })
 export class UserService {
-    constructor(private readonly afAuth: AngularFireAuth) {}
+    constructor(private readonly auth: AngularFireAuth) {}
 
-    login() {}
+    async login(email: string, password: string): Promise<User | null> {
+        const result = await this.auth.signInWithEmailAndPassword(
+            email,
+            password,
+        );
+        return {};
+    }
 }
