25dd01b1e1fcfaa3d43a0afeea8fcc5e6f4d5e8e
niklierik <erik200203@gmail.com>
2023-04-25 23:40:10 +0200

    Removed environments from gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'neptun-copy',
+    appId: '1:261308946328:web:ba99fc887e52ba33c7fded',
+    databaseURL: 'https://neptun-copy-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'neptun-copy.appspot.com',
+    apiKey: 'AIzaSyBw8e44OlYFVG__uzGel9oxk4lbU6xJhZ4',
+    authDomain: 'neptun-copy.firebaseapp.com',
+    messagingSenderId: '261308946328',
+  },
+};
